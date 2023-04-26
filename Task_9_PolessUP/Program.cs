63bd232acbc607fd554a6cca18166fa416c002ec
Дима Случак <pripru916@gmail.com>
2023-04-27 00:18:24 +0300

    Добавьте файлы проекта.

---
+++ Task_9_PolessUP/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_9_PolessUP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите год:");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (year % 4 == 0)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
